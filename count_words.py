b9553adf40a54738a8d63562294808f16e771ab2
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-04-15 12:39:14 +0300

    Add exercise count_words.py

---
+++ count_words.py
@@ -0,0 +1,18 @@
+def count_word(text:str)->dict:
+    """
+    A function that gets text and returns the lengths of words in the text as a dictionary.
+    :param text:Text for which the lengths of the words will be calculated.
+    :return:Lengths of words in the text as a dictionary.
+    """
+    return {word: len(word) for word in text.replace("\n"," ").replace("."," ").split(' ') if word != '' and word != '\n'}
+
+
+if __name__ == '__main__':
+    text_for_count_word = """
+    You see, wire telegraph is a kind of a very, very long cat.
+    You pull his tail in New York and his head is meowing in Los Angeles.
+    Do you understand this?
+    And radio operates exactly the same way: you send signals here, they receive them there.
+    The only difference is that there is no cat.
+    """
+    print((count_word(text_for_count_word)))
