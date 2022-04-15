fcec04a11493a5e35d8031bb97f6041ebca4d188
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-04-15 11:33:12 +0300

    Add exercise running_two_thousand.py

---
+++ running_two_thousand.py
@@ -0,0 +1,21 @@
+import time
+
+
+def timer(f,*args,**kwargs)->time:
+    """
+    A function that receives a function and parameters and return how long time a function f ran when the same parameters are passed to it.
+    :param f:The function for running and calculating its time.
+    :param args:A non key worded variable-length argument list to function f.
+    :param kwargs:A key worded variable-length argument list to function f.
+    :return:How long time a function f ran when the same parameters are passed to it.
+    """
+    start_time = time.time()
+    f(*args,**kwargs)
+    end_time = time.time()
+    return end_time-start_time
+
+
+if __name__ == '__main__':
+    print(timer(print, "Hello"))
+    print(timer(zip, [1, 2, 3], [4, 5, 6]))
+    timer("Hi {name}".format, name="Bug")
