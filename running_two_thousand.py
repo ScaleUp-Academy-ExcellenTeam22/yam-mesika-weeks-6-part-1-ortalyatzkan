056d3fc1effe9bef8e7de5a5745611082f796047
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-05-10 01:18:48 +0300

    Change name of variable and add type hints

--- running_two_thousand.py
+++ running_two_thousand.py
@@ -1,16 +1,18 @@
 import time
+from typing import Tuple, List, Callable
 
 
-def timer(f,*args,**kwargs)->time:
+def timer(f: Callable, *non_keyword_var: Tuple, **keyword_var: Tuple) -> time:
     """
-    A function that receives a function and parameters and return how long time a function f ran when the same parameters are passed to it.
+    A function that receives a function and parameters and return how long time a function f ran when the same parameter
+    s are passed to it.
     :param f:The function for running and calculating its time.
-    :param args:A non key worded variable-length argument list to function f.
-    :param kwargs:A key worded variable-length argument list to function f.
+    :param non_keyword_var:A non key worded variable-length argument list to function f.
+    :param keyword_var:A key worded variable-length argument list to function f.
     :return:How long time a function f ran when the same parameters are passed to it.
     """
     start_time = time.time()
-    f(*args,**kwargs)
+    f(*non_keyword_var, **keyword_var)
     end_time = time.time()
     return end_time-start_time
 
